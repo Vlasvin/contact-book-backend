ea7eb03923a4aa31de202ede88a695bcd9045dc2
Oleksii <103183090+Vlasvin@users.noreply.github.com>
2024-01-24 17:18:11 +0200

    setup env

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
 const contactsRouter = require("./routes/contactsRouter.js");
+require("dotenv").config();
 
 const app = express();
 
--- server 4.js
+++ server 4.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 const app = require("./app");
 
-const { DB_HOST } = process.env;
+const DB_HOST =
+  "mongodb+srv://vlasvin:5s7zPfhALGRsIc8R@cluster0.bv7htjb.mongodb.net/contacts_reader?retryWrites=true&w=majority";
 
 mongoose
   .connect(DB_HOST)
