c86213b2e388bff85910deaf8c2f3b5631fecc32
Oleksii <103183090+Vlasvin@users.noreply.github.com>
2024-02-13 12:41:27 +0200

    first commit

--- middlewares/isFileExist.js
+++ middlewares/isFileExist.js
@@ -1,7 +1,6 @@
 const { HttpError } = require("../helpers");
 
 const isFileExist = (req, res, next) => {
-  console.log(req.file);
   if (!req.file) {
     next(HttpError(400, ` Must add file`));
   }
